6e94b09b1fb963aaad803c73f6f30738ceef4dd9
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-30 12:39:37 +0100

    linking between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,24 @@
 // our-domain/news
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 function NewsPage() {
-    return <h1>The NewsPage</h1>
+    return (
+        <Fragment>
+            <h1>The NewsPage</h1>
+            <ul>
+                <li>
+                    <Link href="/news/great-frame">
+                        NextJs is a great framework
+                    </Link>
+                </li>
+                <li>
+                   Something else
+                </li>
+            </ul>
+        </Fragment>
+    )
 }
 
 export default NewsPage;
