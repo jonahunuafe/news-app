73390e34cea2a45b6356196970df6fa2e51c66fd
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-30 12:16:46 +0100

    extracting dynamic parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,11 @@
 // our-domain/news/something-important
+import { useRouter } from "next/router";
 
 function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
     return <h1>The Detail Page</h1>
 }
 
