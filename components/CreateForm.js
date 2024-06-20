a509a2a7313100c910c796eca3dc75bf7f108f34
Jonah Unuafe <fejisfejiro@gmail.com>
2024-06-20 15:42:37 +0100

    addContact to createForm component

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -1,12 +1,14 @@
 'use client'
 import React from 'react'
 import Input from './Input'
+import { addContact } from '@/lib/action'
 
 
 const CreateForm = () => {
     return (
         <div>
             <form
+            action={addContact}
             className='bg-gray-100 items-center p-2  rounded'
             >
             <div className='flex mb-4'>
