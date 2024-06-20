37e3157a1590509274bbc2371b778f6f1c5fd6af
Jonah Unuafe <fejisfejiro@gmail.com>
2024-06-20 14:58:00 +0100

    setup createpage route in app folder

---
+++ app/create/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Heading from '@/components/Heading'
+import CreateForm from '@/components/CreateForm'
+
+const CreatePage = () => {
+  return (
+    <section >
+        <Heading
+            title="Add Contact"
+            description="Add Contacts Below"
+        />
+        <div className='flex flex-col items-center justify-center'>
+            <CreateForm />
+        </div>
+    </section>
+  )
+}
+
+export default CreatePage
