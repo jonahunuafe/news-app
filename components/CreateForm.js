7dec606b57d507f1bd8ef2e595c28e537df4f79a
Jonah Unuafe <fejisfejiro@gmail.com>
2024-06-29 15:05:51 +0100

    connecting label to input via id's

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -7,46 +7,56 @@ import CreateFormSubmit from './CreateFormSubmit'
 
 const CreateForm = () => {
     return (
-        <div>
+        <div className="w-[90%] md:w-[70%]">
             <form
                 action={addContact}
                 className='bg-gray-100 items-center p-2  rounded'
             >
                 <div className='flex mb-4'>
                     <div className='w-1/2 pr-2'>
-                        <label className='text-gray-700'>Headline</label>
+                        <label htmlFor='headline' className='text-gray-700'>Headline</label>
                         <Input
                         type='text'
+                        id='headline'
                         placeholder="Headline"
                         name='headline'
                         />
                     </div>
                     <div className='w-1/2 pr-2'>
-                        <label className='text-gray-700'>Creator</label>
+                        <label htmlFor='creator' className='text-gray-700'>Creator</label>
                         <Input
                         type='text'
+                        id='creator'
                         placeholder="Creator"
                         name='creator'
                         />
                     </div>
                 </div>
-                <div className='w-full'>
-                    <label>Description</label>
-                    <textarea placeholder="description" name="description" rows='5' />
+                <div className="flex flex-col mb-4">
+                    <label htmlFor='description'>Description</label>
+                    <textarea 
+                        id='description'
+                        className='rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus-outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent'
+                        placeholder='Description' 
+                        name='description' 
+                        rows='10' 
+                    />
                 </div>
                 <div className='flex mb-4'>
                     <div className='w-1/2 pr-2'>
-                        <label className='text-gray-700'>Email</label>
+                        <label htmlFor='email' className='text-gray-700'>Email</label>
                         <Input
                         type='text'
-                        placeholder="Email"
+                        id='email'
+                        placeholder='Email'
                         name='email'
                         />
                     </div>
                     <div className='w-1/2 pr-2'>
-                        <label className='text-gray-700'>Date</label>
+                        <label htmlFor='date' className='text-gray-700'>Date</label>
                         <Input
                         type='date'
+                        id='date'
                         placeholder="Date"
                         name='date'
                         />
--- components/Input.js
+++ components/Input.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Input = ({placeholder, type, name}) => {
+const Input = ({placeholder, type, name, id}) => {
   return (
     <div>
-        <input
-        placeholder={placeholder}
-        type={type}
-        name={name}
-        className='rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus-outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent'
-        />
+      <input
+      placeholder={placeholder}
+      type={type}
+      name={name}
+      id={id}
+      className='rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus-outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent'
+      />
     </div>
   )
 }
