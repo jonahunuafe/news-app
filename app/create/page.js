d2190de46cecd376843436fff9f7e6c2fae2e077
Jonah Unuafe <fejisfejiro@gmail.com>
2024-06-29 14:24:23 +0100

    styling create Page

--- app/create/page.js
+++ app/create/page.js
@@ -6,8 +6,8 @@ const CreatePage = () => {
   return (
     <section >
         <Heading
-          title="Add Contact"
-          description="Add Contacts Below"
+          title="Fill in the Form Below to Make a Post of Latest News."
+          description="News Based on Assumption and False claim is highly Prohibited!"
         />
         <div className='flex flex-col items-center justify-center'>
           <CreateForm />
