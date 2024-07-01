a1f8f13d279602bab20efd804ef3f55a6a604e88
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-01 23:54:34 +0100

    restyled Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,17 +4,22 @@ import {FiPlusCircle, FiHome} from "react-icons/fi"
 
 const Navbar = () => {
   return (
-    <nav className='flex gap-6 justify-between items-center cursor-pointer bg-slate-700 text-white'>
+    <nav className='flex justify-between items-center bg-slate-700 px-8 py-6'>
+      <Link href={'/'} className='text-white font-bold text-base md:text-lg font-serif'>The News Room</Link>
+      <div className='text-white cursor-pointer flex gap-6 md:gap-20 items-center'>
         <Link 
-            className='flex items-center gap-1'
-            href={'/'}>
-            <FiHome /> <span>Home</span>
+          className='flex items-center gap-1 font-medium'
+          href={'/'}
+        >
+          <FiHome /> <span>Home</span>
         </Link>
         <Link 
-            className='flex items-center gap-1'
-            href={'/create'}>
-            <FiPlusCircle /> <span>Create</span>
+          className='flex items-center gap-1 font-medium'
+          href={'/create'}
+        >
+          <FiPlusCircle /> <span>Create</span>
         </Link>
+      </div>
     </nav>
   )
 }
