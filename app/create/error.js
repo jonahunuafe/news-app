058b0cfc564c5d0d80a8d57851074696296ec90b
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-02 09:09:48 +0100

    refactor detailsPage

--- app/create/error.js
+++ app/create/error.js
@@ -4,8 +4,8 @@ import React from 'react'
 
 const error = () => {
   return (
-    <div>
-        <p>
+    <div className='flex flex-col align-middle justify-center bg-red-100 h-[100vh]'>
+        <p className='text-center text-xl md:text-2xl font-medium px-3'>
             Failed to Create News, Please Make Sure all Fields are Filled Before Posting a News
         </p>
     </div>
--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -10,8 +10,8 @@ const DetailsPage = async ({ params }) => {
   const specificNews = await getSpecificNews(id)
 
   return (
-    <div className="flex justify-center flex-col h-[100vh] px-6 md:w-[70%] m-auto">
-      <div className='h-1 w-[70%] bg-orange-400 rounded mx-auto mb-6'></div>
+    <div className="flex justify-center flex-col px-6 md:w-[70%] m-auto">
+      <div className='h-1 w-[70%] bg-orange-400 rounded mx-auto mb-6 mt-10'></div>
       <h2 className="text-center font-bold text-2xl py-6 text-gray-800">{specificNews.headline}</h2>
       <p className="text-gray-800">{specificNews.description}</p>
       <h4 className="text-right text-gray-800 pt-4">Source: {specificNews.creator}</h4>
@@ -28,7 +28,7 @@ const DetailsPage = async ({ params }) => {
         <span className="text-right block text-gray-800">Email: {specificNews.email}</span>
       </div>
       <Link href="/" className="text-center font-medium mt-7 text-green-600 border-lime-500 border-2 w-[max-content] mx-auto px-4 py-2 rounded-full">Back to Home Page</Link>
-      <div className='h-1 w-[70%] mx-auto bg-orange-400 rounded mt-10'></div>
+      <div className='h-1 w-[70%] mx-auto bg-orange-400 rounded my-10'></div>
     </div>
   )
 }
