11cca79ade10bb165b968b59392312096fd16bcf
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-03 09:02:47 +0100

    refactor error.js file

--- app/create/error.js
+++ app/create/error.js
@@ -5,6 +5,9 @@ import React from 'react'
 const error = () => {
   return (
     <div className='flex flex-col align-middle justify-center bg-red-100 h-[100vh]'>
+        <h1 className='text-center text-xl md:text-2xl font-medium px-3'>
+            An error occured
+        </h1>
         <p className='text-center text-xl md:text-2xl font-medium px-3'>
             Failed to Create News, Please Make Sure all Fields are Filled Before Posting a News
         </p>
