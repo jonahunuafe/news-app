8109a34dfbe654095fa0c3b82f32eb2644e97f4e
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-03 09:00:32 +0100

    added not-found page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className=''>
+            <h1>News not found</h1>
+            <p>Unfortunately, we could not find the requested page or News data.</p>
+        </main>
+    )
+}
