c887af5c797441123b91495c01a2c549cdce9b82
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-03 09:25:04 +0100

    styled error and not-found page

--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,12 @@
 export default function NotFound() {
     return (
-        <main className=''>
-            <h1>News not found</h1>
-            <p>Unfortunately, we could not find the requested page or News data.</p>
+        <main className="flex flex-col align-middle justify-center gap-6 pt-12">
+            <h1 className="text-center text-red-500 text-lg md:text-xl font-medium">
+                News not found
+            </h1>
+            <p className="text-center text-red-500 text-lg md:text-xl px-4 font-medium">
+                Unfortunately, we could not find the requested page or News data.
+            </p>
         </main>
     )
 }
