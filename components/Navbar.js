704baa90c63a3ca70fd076f6e10715f2cb0b9145
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-10 04:55:33 +0100

    refactor styling of Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,29 +11,31 @@ const Navbar = async () => {
   return (
     <nav className='flex flex-col md:flex-row justify-between items-center bg-zinc-800 px-8 py-6'>
       <Link href={'/'} className='text-white font-bold text-base md:text-lg font-serif border-b-2 md:border-b-0 pb-1 md:pb-0'>The News Room</Link>
-      <div className='text-white flex justify-between items-center w-full md:w-1/4 lg:w-1/3 pt-4 pb-1 md:pt-0 md:pb-0'>
-        <Link 
-          className='flex items-center gap-1 font-medium cursor-pointer'
-          href={'/'}
-        >
-          <FiHome /> <span>Home</span>
-        </Link>
-        <Link 
-          className='flex items-center gap-1 font-medium cursor-pointer'
-          href={'/create'}
-        >
-          <FiPlusCircle /> <span>Create</span>
-        </Link>
-      </div>
-      <div>
+      <div className='flex flex-col justify-between gap-5 md:gap-x-10 md:flex-row w-full md:w-8/12 lg:w-6/12'>
+        <div className='flex justify-between w-full md:w-80 lg:w-60'>
+          <Link 
+            className='flex items-center gap-1 font-medium text-white cursor-pointer'
+            href={'/'}
+          >
+            <FiHome /> <span>Home</span>
+          </Link>
+          <Link 
+            className='flex items-center gap-1 text-white font-medium cursor-pointer'
+            href={'/create'}
+          >
+            <FiPlusCircle /> <span>Create</span>
+          </Link>
+        </div>
         {
           user ? 
-            <div>
-              <p>{user.given_name}</p>
+            <div className='flex justify-between w-full md:w-[90%] lg:w-72'>
+              <p className='text-white'>
+                Logged in as <span className='text-orange-300 font-semibold'>{user.given_name}</span>
+              </p>
               <LogoutLink className='text-white font-medium cursor-pointer'>Logout</LogoutLink>
             </div>
             : (
-            <div className='flex gap-10'>
+            <div className='flex justify-between w-full pt-4 pb-1 md:pt-0 md:pb-0'>
               <LoginLink className='text-white font-medium cursor-pointer'>
                 Login in
               </LoginLink>
