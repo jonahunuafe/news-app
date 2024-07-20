f1cc11efa4d72692b606a9bb129bd32684c5e32f
Jonah Unuafe <fejisfejiro@gmail.com>
2024-07-20 19:33:30 +0100

    set up cloudinary in nextConfig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    env: {
+        NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dn7lmikzs",
+        NEXT_PUBLIC_CLOUDINARY_PRESET_NAME: "p3jubona"
+    },
+};
 
 export default nextConfig;
